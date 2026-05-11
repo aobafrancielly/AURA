64e6fed68bd0bc981b74a2a21d02c81be8177232
Francielly Silva <143453550+aobafrancielly@users.noreply.github.com>
2026-05-11 15:52:56 -0300

    Delete assets directory

--- assets/js/noivado.js
+++
@@ -1,9 +0,0 @@
-const loadMoreBtn = document.querySelector('#loadMore');
-const hiddenItems = document.querySelectorAll('.product-card.hidden');
-
-loadMoreBtn.addEventListener('click', () => {
-    hiddenItems.forEach(item => {
-        item.classList.add('show-magic');
-    });
-    loadMoreBtn.style.display = 'none';
-});
